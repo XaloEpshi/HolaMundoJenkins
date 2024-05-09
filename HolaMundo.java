0512a7d146a2878189969e77f0037eb6684bb931
GonDev <gonzalo.mellao.m@gmail.com>
2024-05-09 00:33:05 -0400

    Primer commit

---
+++ HolaMundo.java
@@ -0,0 +1,11 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        String nombre = "Gonzalo";
+        System.out.println("Hola, " + nombre + "! Bienvenido al programa HolaMundo.");
+
+        int num1 = 3, num2 = 3;
+        System.out.println("Vamos a realizar un cálculo simple. ¿Cuánto es " + num1 + " * " + num2 + "?");
+        int resultado = num1 * num2;
+        System.out.println("El resultado de " + num1 + " * " + num2 + " es " + resultado + ".");
+    }
+}
